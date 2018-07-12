1bee8769f8f9e421ce02ab55b052388db5e5dae7
Jordan Blakey <jordan.t.blakey@gmail.com>
2018-07-11 21:42:30 -0500

    Firebase project migration

---
+++ public/assets/js/firebase-status.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    let app = firebase.app()
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(
+      feature => typeof app[feature] === 'function'
+    )
+    document.getElementById(
+      'load'
+    ).innerHTML = `Firebase SDK loaded with ${features.join(', ')}`
+
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION) // SESSION, LOCAL
+    firebase.firestore().settings({ timestampsInSnapshots: true })
+  } catch (e) {
+    console.error(e)
+    document.getElementById('load').innerHTML =
+      'Error loading the Firebase SDK, check the console.'
+  }
+})
