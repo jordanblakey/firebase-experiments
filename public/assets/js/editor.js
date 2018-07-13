1b2ce40184afa80d9e8a4dd39f08b745033f041a
Jordan Blakey <jordan.t.blakey@gmail.com>
2018-07-13 03:25:00 -0500

    Added Realtime Database Explorer, Zen Mode to Editor, Preview Mode keyboard shortcuts, some animations and a delete all data button that's not working yet.

--- public/assets/js/editor.js
+++ public/assets/js/editor.js
@@ -1,27 +1,105 @@
 function renderEditor(editorContainer) {
   editorContainer.innerHTML = `
-    <textarea id="editor">
-# Scorched Uses Markdown
-
-----
-
-> The idea is that a [Markdown](https://www.markdownguide.org/cheat-sheet) formatted document should be **publishable as-is**, as plain text, *without looking like it’s been marked up with tags or formatting instructions.*
-    </textarea>
+    <textarea id="editor"></textarea>
     <button class="save-note-button" onclick="saveNote(editor)">Log Markdown to Console</button>
+    <span class="editorInstructions">ctrl+shift+z: Toggle Zen</span>
+    <span class="editorInstructions">ctrl+shift+z: Toggle Preview</span>
   `
-  const editor = new Editor({
-    element: document.getElementById('#editor')
+
+  window.editor = new Editor({
+    element: document.getElementById('#editor'),
     // toolbar: [], // array or false
-    // status: [] // array or false
+    status: ['lines'] // array or false
     // actions: {},
     // shortcuts: {}
   })
 
-  editor.render()
+  window.editor.render()
+  configEditor()
+}
 
-  saveNote = () => {
-    // Get the value out of the editor
-    let val = editor.codemirror.getValue()
-    console.log(val)
+////////////////////////////////////////////////////////////////////////////////
+
+function saveNote() {
+  // Get the full value out of the editor
+  // let val = window.editor.codemirror.getValue()
+  // console.log(val)
+
+  // Parse Title
+  let noteTitle = window.editor.codemirror.getLine(0)
+  console.log('Note Title:', noteTitle)
+
+  // Parse Body
+  let lines = []
+
+  window.editor.codemirror
+    .getDoc(0)
+    .children[0].lines.forEach(
+      (line, i) => (i > 0 ? lines.push(line.text) : null)
+    )
+  let noteBody = lines.join('\n')
+  console.log('Note Body:', noteBody)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// CONFIGURE EDITOR
+////////////////////////////////////////////////////////////////////////////////
+
+function configEditor() {
+  window.editor.codemirror.setOption('tabSize', 2)
+  window.editor.codemirror.setOption('lineWrapping', true)
+  window.editor.codemirror.setOption('cursorBlinkRate', 99999999999999999)
+  window.editor.codemirror.setOption('cursorHeight', 1)
+  window.editor.redo() // Focuses the editor
+
+  // window.editor.codemirror.doc.cm.on('focus', () =>
+  //   window.editor.codemirror.doc.cm.execCommand('selectAll')
+  // )
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// KEYBOARD SHORTCUTS
+////////////////////////////////////////////////////////////////////////////////
+
+// MAP KEY FOR ZEN MODE
+window.zenMode = false
+window.previewMode = false
+window.addEventListener('keydown', checkKeyPressed, false)
+function checkKeyPressed(e) {
+  if (e.keyCode === 90 && e.ctrlKey && e.shiftKey) {
+    if (window.zenMode === false) {
+      window.zenMode = true
+      window.editor.toggleFullScreen()
+    } else {
+      window.zenMode = false
+      try {
+        document.exitFullscreen()
+      } catch (err) {
+        console.log(null)
+      }
+      try {
+        document.webkitExitFullscreen()
+      } catch (err) {
+        console.log(null)
+      }
+    }
+  } else if (e.keyCode === 88 && e.ctrlKey && e.shiftKey) {
+    if (window.previewMode === false) {
+      window.previewMode = true
+      window.editor.togglePreview()
+    } else {
+      window.previewMode = false
+      window.editor.togglePreview()
+    }
   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// DEBUG TOOLS
+////////////////////////////////////////////////////////////////////////////////
+
+// LOG KEYS
+window.addEventListener('keydown', logKeys, false)
+function logKeys(e) {
+  console.log(e)
 }
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -9,18 +9,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function renderPage() {
   let pageContent = document.querySelector('#pageContent')
-  let extraContent = document.querySelector('#extraContent')
+  pageContent.innerHTML = ''
+
+  let body = document.querySelector('body')
+  body.insertAdjacentHTML('afterbegin', '<div id="splash"></div>')
+
+  let editorContainer = document.querySelector('#editorContainer')
+  renderEditor(editorContainer)
 
   let saveNote
-  renderEditor(document.querySelector('#editorContainer'))
+  let noteContainer = document.querySelector('#noteContainer')
+  noteContainer.innerHTML = '<h2>All Notes</h2>'
+  renderNotes(noteContainer)
 
-  pageContent.innerHTML = '<h2>All Notes</h2>'
-  renderNotes(pageContent)
-  renderScorch(pageContent)
-
+  let extraContent = document.querySelector('#extraContent')
   extraContent.innerHTML = '<h2>Unfinished Features</h2>'
   fileUploader(extraContent)
   scratchPad(extraContent)
+  renderRealtimeDatabaseExplorer(extraContent)
+  renderScorch(extraContent)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,14 +47,18 @@ async function renderNotes(targetElem) {
         targetElem.innerHTML += `
           <div class="note" id="${doc}">
             <div class="noteHeader">
-              <h1 class="title">${snap.data().title}</h1>
+              <h1 class="title" contenteditable="true" onblur="console.log(this.innerHTML)">${
+                snap.data().title
+              }</h1>
               <div>
                 <button class="noteButton" onclick="updateNote('${doc}')">🖉</button>
                 <button class="noteButton delete" onclick="deleteNote('${doc}')">🗑️</button>
               </div>
             </div>
 
-            <p class="body">${snap.data().body}</p>
+            <p class="body" contenteditable="true" onblur="console.log(this.innerHTML)">${
+              snap.data().body
+            }</p>
             <code class="createdAt">${snap.data().createdAt}</code>
           </div>
         `
@@ -115,79 +126,4 @@ function deleteNote(docId) {
     .catch(function(error) {
       console.error('Error removing document: ', error)
     })
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// SCRATCHPAD
-////////////////////////////////////////////////////////////////////////////////
-
-function scratchPad(targetElem) {
-  targetElem.innerHTML += `
-    <div class="scratchpad" id="sp1">
-      <h3>Scratchpad</h3>
-      <p>
-        ☐ Title field for Editor<br>
-        ☐ Generate prefixed UID<br>
-        ☐ Save to FB as doc:UID - Editor and title contents, w/current timestamp<br>
-        ☐ Fix saving to FB with newlines<br>
-        ☐ User Profile card or modal<br>
-        ☐ Import Realtime Database explorer<br>
-        ☐ Create Firebase explorer<br>
-      </p>
-    </div>
-  `
-
-  const productsRef = firebase.firestore().collection('products')
-
-  let query
-  query = productsRef.where('price', '>', 10)
-  // query = productsRef.orderBy('price', 'desc')
-  // query = productsRef.orderBy('price', 'desc').limit(1)
-  query.get().then(products => {
-    products.forEach(doc => {
-      data = doc.data()
-      document.querySelector('#sp1').innerHTML += `${data.name} at $${
-        data.price
-      }<br>`
-    })
-  })
-}
-
-// //////////////////////////////////////////////////////////////////////////////
-// FILE UPLOADER
-// //////////////////////////////////////////////////////////////////////////////
-
-function fileUploader(targetElem) {
-  targetElem.innerHTML += `
-    <div class="scratchpad" id="sp2">
-      <h3>FB Storage Upload</h3>
-      <input type="file" onchange="uploadFile(this.files)">
-      <hr>
-      <img id="imgUpload" src="" width="100%" />
-    </div>
-  `
-}
-
-function uploadFile(files) {
-  const file = files.item(0)
-  const ref = firebase
-    .storage()
-    .ref()
-    .child(file.name)
-  const img = document.querySelector('#imgUpload')
-  ref.put(file).then(ref.getDownloadURL().then(url => (img.src = url)))
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// FILE UPLOADER
-////////////////////////////////////////////////////////////////////////////////
-
-function renderScorch(targetElem) {
-  targetElem.innerhtml += `
-    <button class="noteButton" onclick="scorch()">SCORCH</button>
-  `
-  function scorch() {
-    let body = document.querySelector('body')
-    body.innerHTML = body.innerHTML.replaceAll(/N/, 'G')
-  }
 }
